2da4b2130e6727b12d6f63f249b37bf4913a8d68
Sami Kojo <sami.kojo@gmail.com>
2017-10-12 12:11:32 +0300

    Move

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,21 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
+		[SerializeField]
+		private float _speed = 5;
+
 		void Update()
 		{
+			var horizontal = Input.GetAxis("Horizontal");
+			var vertical = Input.GetAxis("Vertical");
+
+			Move(new Vector3(horizontal, 0, vertical));
+		}
+
+		private void Move(Vector3 input)
+		{
+			Vector3 movementVector = input * _speed;
+			transform.Translate(movementVector * Time.deltaTime);
 		}
 	}
 }
