5635fccc40c104a48ca69ada5ffef4631438b81e
Sami Kojo <sami.kojo@gmail.com>
2017-10-12 12:24:31 +0300

    Jump

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,49 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
+		[SerializeField]
+		private float _jumpForce = 100;
+
+		private bool _isGrounded = true;
+		private Rigidbody _rigidbody;
+
+		void Awake()
+		{
+			_rigidbody = GetComponent<Rigidbody>();
+		}
+
 		void Update()
 		{
+			bool jump = Input.GetButtonDown("Jump");
+
+			if (jump)
+			{
+				Jump();
+			}
+		}
+
+		private void Jump()
+		{
+			if (_isGrounded)
+			{
+				_rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+			}
+		}
+
+		private void OnCollisionEnter(Collision collision)
+		{
+			if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+			{
+				_isGrounded = true;
+			}
+		}
+
+		private void OnCollisionExit(Collision collision)
+		{
+			if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+			{
+				_isGrounded = false;
+			}
 		}
 	}
 }
