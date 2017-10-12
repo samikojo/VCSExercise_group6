36e4444dad3792b8a224c31bd62ef0cc856f695c
Sami Kojo <sami.kojo@gmail.com>
2017-10-12 12:17:15 +0300

    Turn + text assets

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,19 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
+		[SerializeField]
+		private float _rotationSpeed = 30;
+
 		void Update()
 		{
+			var turn = Input.GetAxis("Turn");
+
+			Turn(turn);
+		}
+
+		private void Turn(float turn)
+		{
+			transform.Rotate(Vector3.up * _rotationSpeed * turn * Time.deltaTime);
 		}
 	}
 }
